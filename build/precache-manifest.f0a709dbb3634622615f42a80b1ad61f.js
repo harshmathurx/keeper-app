aec7eb8a0817442046b45832568842031e880b32
harshmathurx <harshmathurx@gmail.com>
2021-08-15 19:28:01 +0530

    inital commit

---
+++ build/precache-manifest.f0a709dbb3634622615f42a80b1ad61f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239061aab4d0717afd7bcc90568ebd9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc4a030d3c32f60eb546",
+    "url": "/static/js/2.8066344a.chunk.js"
+  },
+  {
+    "revision": "5204603f890f2e5c6267",
+    "url": "/static/js/main.8d7b22ec.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Made by <a href="https://github.com/harshmathurx">Harsh Mathur</a></p>
+    </footer>
+  );
+}
+
+export default Footer;
